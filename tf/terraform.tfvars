6f5052d5cdbabecc8abfc3f292662a42c976ca88
Sean Youngberg <sean.youngberg@gmail.com>
2022-09-16 07:43:28 -0500

    update vars and guide

--- tf/terraform.tfvars
+++ tf/terraform.tfvars
@@ -1,6 +1,6 @@
 cluster_name = "lab-k8s-cn-v3"
 vpc_name  = "vpc-cn-v3"
-kubeconfig_output_path = "~/.kube/config-eks"
+kubeconfig_output_path = "/home/cloudshell-user/.kube/config"
 region = "us-west-2"
 key_pair = ""
-owner = ""
+owner = "lab"
--- tf/variables.tf
+++ tf/variables.tf
@@ -20,15 +20,6 @@ variable "region" {
 
 variable "safe_ips" {
   default = [
-    "8.47.64.2/32",         #hq
-    "34.99.77.241/32",      #prisma nl central
-    "34.99.77.242/32",      #prisma nl central
-    "54.241.37.235/32",     #prisma us west
-    "83.242.74.253/32",     #amsterdam-gw
-    "84.207.227.0/28",      #amsterdam-gw
-    "84.207.230.24/29",     #amsterdam lab
-    "134.238.0.0/16",       #pan /16
-    "213.39.97.34/32",      #amsterdam-gp
     "0.0.0.0/0"             #everywhere for QwikLabs :)
   ]
   type = list
